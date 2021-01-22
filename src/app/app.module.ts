3ac1d67eed175fe3f4b4442b71226ec4f2db8626
DieysonRandeTi <dieyson99103660@gmail.com>
2021-01-22 19:35:12 -0300

    Subindo o Projeto

---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import {ROUTES} from './app.routes';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
+import { RestaurantService } from './restaurants/restaurant/restaurants.service';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from '../app/restaurant-detail/menu/menu.component';
+import { ShoppingCartComponent } from '../app/restaurant-detail/shopping-cart/shopping-cart.component';
+import { MenuItemComponent } from '../app/restaurant-detail/menu-item/menu-item.component';
+import { ReviewsComponent } from '../app/restaurant-detail/reviews/reviews.component';
+import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
+import { OrderComponent } from './order/order.component';
+import { FormsModule } from '@angular/forms';
+import { InputComponent } from '../app/shared/input/input.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    RestaurantsComponent,
+    RestaurantComponent,
+    RestaurantDetailComponent,
+    MenuComponent,
+    ShoppingCartComponent,
+    MenuItemComponent,
+    ReviewsComponent,
+    OrderComponent,
+    InputComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot(ROUTES)
+  ],
+  providers: [RestaurantService, ShoppingCartService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
